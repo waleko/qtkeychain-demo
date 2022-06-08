3b88ffab22096bb6570314dd79b0675481ee7881
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-06-08 20:41:01 +0300

    add some output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
+
 #include <keychain.h>
 
 int main() {
     QKeychain::WritePasswordJob j("qtkeychain-test");
     j.start();
+    
+    std::cout << "OK!" << std::endl;
 
     return 0;
 }
